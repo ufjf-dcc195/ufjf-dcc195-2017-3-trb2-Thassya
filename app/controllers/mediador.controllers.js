1384256389bf8f15a13d669d18a96f26ea8b51be
Thassya <thassya@ice.ufjf.br>
2017-11-29 01:32:40 -0200

    amigo oculto

--- app/controllers/mediador.controllers.js
+++ app/controllers/mediador.controllers.js
@@ -1,10 +1,23 @@
 var listanomes;
 
 module.exports.amigooculto = function(req,res,next){
-    if(req.method=='GET'){        
-        console.log(req.body);
-        res.render("amigooculto/mediador", {nomes: listanomes});
-    }
+    if(req.method=='GET'){
+        console.log("GET");
+        res.render("amigooculto/mediador")
+      }else if(req.method=='POST'){
+        var nomes = req.body.nomes.split(",");
+        var pares = []
+        var contaPar=0;
+        var numParticipantes = nomes.length
+        for(var i=0;i<numParticipantes-1;i++){
+          pares[contaPar]={ 'nome1': nomes[i],'nome2': nomes[i+1]}
+          contaPar++;
+        }
+        pares[contaPar]={nome1:nomes[numParticipantes-1],nome2: nomes[0]}
+
+        console.log("posttt");
+        res.render("amigooculto/resultado",{amigos: pares})
+      }
 };
 
 module.exports.adicionaNome = function(req,res){
--- app/routes/amigooculto.routes.js
+++ app/routes/amigooculto.routes.js
@@ -1,6 +1,5 @@
 module.exports = function (app){
     var handlers = require("../controllers/mediador.controllers");
     app.use("/amigooculto.html", handlers.amigooculto);
-    app.post("/adicionaNome", handlers.adicionaNome);
 };
   
